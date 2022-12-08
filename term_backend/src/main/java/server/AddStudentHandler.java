e23c78bdebcf634ebe0ec4a37e049029a600fc6a
Aanya Hudda <aanyahudda@aanyas-air.devices.brown.edu>
2022-12-08 12:36:55 -0500

    feat: addStudents handler almost finished, just need to uncomment the actual additions to the database

--- term_backend/src/main/java/server/AddStudentHandler.java
+++ term_backend/src/main/java/server/AddStudentHandler.java
@@ -36,7 +36,6 @@ public class AddStudentHandler implements Route{
 
   public List<String> handleTables(String studentName, String studentEmail, String studentYear, String className){
     List<String> studentWaitlist = new ArrayList<String>();
-
     try {
       Class.forName("org.sqlite.JDBC");
       String urlToDB = "jdbc:sqlite:" + "waitlist.sqlite3";
@@ -58,111 +57,33 @@ public class AddStudentHandler implements Route{
         studentID = rs.getInt(1);
       }
 
-      if(studentExistsInStudents){
+      // If student doesn't exist in "students" table, add student to it
+      if(studentExistsInStudents == false){
+        System.out.println("student " + studentName + " does not exist in 'students' table");
+        studentID = this.addtoStudents(prep, conn, rs, studentName);
+        System.out.println("student " + studentName + " was added to 'students' table at " + studentID);
+      } else {
         System.out.println("student " + studentName + " already exists in 'students table");
       }
 
-      // If student doesn't exist in "students" table, add student to it
-      Integer maxStudentID = 0;
-      if(studentExistsInStudents == false){
-        System.out.println("student " + studentName + " does not exist in 'students' table");
-        // Find max student_id
-        prep = conn.prepareStatement("SELECT MAX(student_id) from students");
-        rs = prep.executeQuery();
-        while(rs.next()){
-          maxStudentID = rs.getInt(1);
-          studentID = maxStudentID + 1;
-        }
-
-        // Insert new student into "students" table
-        /*
-        prep = conn.prepareStatement(
-            "INSERT INTO students VALUES (?, ?, ?, ?);");
-
-        prep.setInt(1, studentID);
-        prep.setString(2, studentName);
-        prep.setString(3, studentEmail);
-        prep.setString(4, studentYear);
-        prep.addBatch();
-        prep.executeBatch();
-         */
-        System.out.println("student " + studentName + " was added to 'students' table at " + studentID);
-      }
-
       // Get class_id from "classes" table
-      prep = conn.prepareStatement("select * from classes WHERE title = ?");
-      prep.setString(1, className);
-      rs = prep.executeQuery();
-      String classID = "";
-
-      while(rs.next()){
-        classID = rs.getString(1);
-      }
+      String classID = this.getClassID(prep, conn, rs, className);
       System.out.println("the class " + className + " has classID " + classID);
 
-      // Check if student exists in "enrollments" table
-      prep = conn.prepareStatement("select * from enrollments WHERE student_id = ?");
-      prep.setString(1, studentID.toString());
-      rs = prep.executeQuery();
-      Boolean studentExistsInEnrollmentsWithCorrectClass = false;
-
-      while(rs.next()){
-        String currClassID = rs.getString(3);
-        if(currClassID.equals(classID)){
-          studentExistsInEnrollmentsWithCorrectClass = true;
-        }
-      }
+      // Check if student exists in "enrollments" table with the correct course
+      Boolean studentExistsInEnrollmentsWithCorrectClass = this.checkIfStudentExistsInEnrollments(prep, conn, rs, studentID, classID);
 
       // If student doesn't exist in "enrollments" with this specific course then add them
-      Integer enrollID = 0;
       if(studentExistsInEnrollmentsWithCorrectClass == false){
         System.out.println("student "+ studentName + " does not exist correctly with class " + className + " in 'enrollments'");
-        System.out.println("so, studentID " + studentID + " does not exist correctly with class " + classID + " in 'enrollments'");
+        this.addToEnrollments(prep, conn, rs, studentID, classID);
+      } else {
+        System.out.println("student already exists in 'enrollments' table with the correct course");
 
-        // Find max enrollment_id
-        prep = conn.prepareStatement("SELECT MAX(enroll_id) from enrollments");
-        rs = prep.executeQuery();
-        while(rs.next()){
-          enrollID = rs.getInt(1);
-          enrollID = enrollID + 1;
-        }
-
-        // Insert new student/course pair into 'enrollments' table
-        /*
-        prep = conn.prepareStatement(
-        "INSERT INTO enrollments VALUES (?, ?, ?);");
-
-        prep.setInt(1, enrollID);
-        prep.setInt(2, studentID);
-        prep.setInt(3, classID);
-        prep.addBatch();
-        prep.executeBatch();
-         */
-
-        System.out.println("studentID " + studentID + " and classID " + classID + " were added to 'enrollments' at enrollID " + enrollID);
       }
 
       // Return: list of names of students in waitlist in order for that specific course
-      prep = conn.prepareStatement("select * from enrollments WHERE class_id = ?");
-      prep.setString(1, classID);
-      rs = prep.executeQuery();
-
-      List<String> studentIDList = new ArrayList<String>();
-      while(rs.next()){
-        studentIDList.add(rs.getString(2));
-      }
-      System.out.println("created a list of relevant student IDs: " + studentIDList);
-
-      prep = conn.prepareStatement("select * from students WHERE student_id = ?");
-      for (int i = 0; i < studentIDList.size(); i++){
-        prep.setString(1, studentIDList.get(i));
-        rs = prep.executeQuery();
-
-        while(rs.next()){
-          studentWaitlist.add(rs.getString(2));
-        }
-      }
-      System.out.println("created list of student names to be returned: " + studentWaitlist);
+      studentWaitlist = this.createReturnedWaitlist(prep, conn, rs, classID);
 
     } catch (SQLException e){
       System.out.println("caught this exception: " + e);
@@ -171,13 +92,6 @@ public class AddStudentHandler implements Route{
     }
 
     return studentWaitlist;
-
-
-    // If the student doesn't exist in "students" table, add the student there
-    // Get the class id from the "classes" table using the class name
-    // If the student's ID doesn't exist in the "enrollments" table, add the student there with the class_ID
-    // Return: list of names of students in waitlist in order for that specific course
-
   }
 
   private String getClassID(PreparedStatement prep, Connection conn, ResultSet rs, String className)
@@ -191,5 +105,101 @@ public class AddStudentHandler implements Route{
       classID = rs.getString(1);
     }
     return classID;
+  }
+
+  private Boolean checkIfStudentExistsInEnrollments(PreparedStatement prep, Connection conn, ResultSet rs, Integer studentID, String classID)
+      throws SQLException {
+    prep = conn.prepareStatement("select * from enrollments WHERE student_id = ?");
+    prep.setString(1, studentID.toString());
+    rs = prep.executeQuery();
+    Boolean studentExistsInEnrollmentsWithCorrectClass = false;
+
+    while(rs.next()){
+      String currClassID = rs.getString(3);
+      if(currClassID.equals(classID)){
+        studentExistsInEnrollmentsWithCorrectClass = true;
+      }
+    }
+    return studentExistsInEnrollmentsWithCorrectClass;
+  }
+
+  private Integer addtoStudents(PreparedStatement prep, Connection conn, ResultSet rs, String studentName)
+      throws SQLException {
+    Integer maxStudentID = 0;
+    Integer studentID = 0;
+    // Find max student_id
+    prep = conn.prepareStatement("SELECT MAX(student_id) from students");
+    rs = prep.executeQuery();
+    while(rs.next()){
+      maxStudentID = rs.getInt(1);
+      studentID = maxStudentID + 1;
+    }
+
+    // Insert new student into "students" table
+        /*
+        prep = conn.prepareStatement(
+            "INSERT INTO students VALUES (?, ?, ?, ?);");
+
+        prep.setInt(1, studentID);
+        prep.setString(2, studentName);
+        prep.setString(3, studentEmail);
+        prep.setString(4, studentYear);
+        prep.addBatch();
+        prep.executeBatch();
+         */
+    return studentID;
+  }
+
+  private void addToEnrollments(PreparedStatement prep, Connection conn, ResultSet rs, Integer studentID, String classID)
+      throws SQLException {
+    Integer enrollID = 0;
+
+    // Find max enrollment_id
+    prep = conn.prepareStatement("SELECT MAX(enroll_id) from enrollments");
+    rs = prep.executeQuery();
+    while(rs.next()){
+      enrollID = rs.getInt(1);
+      enrollID = enrollID + 1;
+    }
+
+    // Insert new student/course pair into 'enrollments' table
+        /*
+        prep = conn.prepareStatement(
+        "INSERT INTO enrollments VALUES (?, ?, ?);");
+
+        prep.setInt(1, enrollID);
+        prep.setInt(2, studentID);
+        prep.setInt(3, classID.toInt());
+        prep.addBatch();
+        prep.executeBatch();
+         */
+    System.out.println("studentID " + studentID + " and classID " + classID + " were added to 'enrollments' at enrollID " + enrollID);
+  }
+
+  private List<String> createReturnedWaitlist(PreparedStatement prep, Connection conn, ResultSet rs, String classID)
+      throws SQLException {
+    List<String> studentWaitlist = new ArrayList<String>();
+    List<String> studentIDList = new ArrayList<String>();
+
+    prep = conn.prepareStatement("select * from enrollments WHERE class_id = ?");
+    prep.setString(1, classID);
+    rs = prep.executeQuery();
+
+    while(rs.next()){
+      studentIDList.add(rs.getString(2));
+    }
+    System.out.println("created a list of relevant student IDs: " + studentIDList);
+
+    prep = conn.prepareStatement("select * from students WHERE student_id = ?");
+    for (int i = 0; i < studentIDList.size(); i++){
+      prep.setString(1, studentIDList.get(i));
+      rs = prep.executeQuery();
+
+      while(rs.next()){
+        studentWaitlist.add(rs.getString(2));
+      }
+    }
+    System.out.println("created list of student names to be returned: " + studentWaitlist);
+    return studentWaitlist;
   }
 }
