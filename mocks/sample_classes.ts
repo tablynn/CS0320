1efeabe8264cc82709947c75f363685f334a18be
Calvineng72 <calvineng72@gmail.com>
2022-12-09 15:49:50 -0500

    fix: course descriptions

--- mocks/sample_classes.ts
+++ mocks/sample_classes.ts
@@ -2,18 +2,26 @@ const classes = [
     {
       courseName: 'CSCI 0320: Introduction to Software Engineering',
       professor: 'Tim Nelson',
+      description:
+        'Focuses on designing, building, testing, and maintaining systems collaboratively.',
     },
     {
       courseName: 'CSCI 0300: Fundamentals of Computer Systems',
       professor: 'Malte Schwarzkopf',
+      description:
+        'Covers fundamental concepts, principles, and abstractions that underlie the design and engineering of computer systems.',
     },
     {
       courseName: 'CSCI 0190: Accelerated Introduction to CS',
       professor: 'Tim Nelson',
+      description:
+        'A one-semester introduction to CS covering programming integrated with core data structures, algorithms, and analysis techniques.',
     },
     {
       courseName: 'CSCI 1470: Deep Learning',
       professor: 'Ritambhara Singh',
+      description:
+        'In this course, you will get an overview of the prominent techniques of deep learning and their applications.',
     }
 ];
 
